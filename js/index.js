b5f245bf2953f650096cb4ddd13b278fca179005
unknown <boodyhesham001@gmail.com>
2024-05-20 15:19:54 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+
+var users =[
+    {name:'ahmed',age:20,salary:1000},
+    {name:'mohamed',age:30,salary:3000},
+    {name:'ali',age:30,salary:5000},
+]
+
+
+for(var i=0;i<users.length;i++){
+    console.log(users[i].name+" "+users[i].age+" "+users[i].salary)
+}
